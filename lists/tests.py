3ef75536b327a20dc4d198010f55fb88c0c70cc9
AjinkyaZ <ajinkya.zadbuke@gmail.com>
2016-06-14 02:30:26 +0530

    Home page HTML is now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from views import home_page
 
@@ -12,6 +13,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+        self.assertTrue(any(row.text == '1: Buy Rabbit feet' for row in rows),
+            "New to-do item did not appear in table")
+    
